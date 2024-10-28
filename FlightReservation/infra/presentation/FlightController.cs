3de793437ef2aa3e7eb69b5d51645aa8cb87984b
ali <ali77erfagh99@gmail.com>
2024-10-28 16:51:23 +0330

    add controller method

--- FlightReservation/infra/presentation/FlightController.cs
+++ FlightReservation/infra/presentation/FlightController.cs
@@ -1,3 +1,4 @@
+using FlightReservation.domain.useCase;
 using FlightReservation.infra.presentation.dto;
 using FlightReservation.models;
 using Microsoft.AspNetCore.Mvc;
@@ -6,13 +7,22 @@ namespace FlightReservation.infra.presentation;
 
 [ApiController]
 [Route("(api/vi/flights)")]
-public class FlightController : ControllerBase
+public class FlightController(AddFlightByAdmin addFlightByAdmin) : ControllerBase
 {
-    
     [HttpPost]
     public async Task<IActionResult> Create([FromBody] FlightDto dto)
     {
-        
+        var flightResponse = await addFlightByAdmin.add(dto);
         return Ok();
+        // return CreatedAtAction(nameof(GetFlightById), new { id = flightResponse.Id }, flightResponse);
+
     }
+    // [HttpGet("{id}")]
+    // public async Task<IActionResult> GetFlightById(int id)
+    // {
+    //     // Retrieve the flight by ID and return it
+    //     var flight = await addFlightByAdmin.GetFlightById(id);
+    //     if (flight == null) return NotFound();
+    //     return Ok(flight);
+    // }
 }
