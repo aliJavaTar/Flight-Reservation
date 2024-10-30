97d7b5e3e314e2550a1c320c4fec7eca0771f8a1
ali <ali77erfagh99@gmail.com>
2024-10-30 09:40:35 +0330

    add user model

--- FlightReservation/infra/data/DataBase.cs
+++ FlightReservation/infra/data/DataBase.cs
@@ -1,6 +1,7 @@
 using FlightReservation.domain;
 using FlightReservation.domain.flight;
 using FlightReservation.domain.ticket;
+using FlightReservation.infra.models;
 using Microsoft.EntityFrameworkCore;
 
 namespace FlightReservation.infra.data;
@@ -10,29 +11,44 @@ public class DataBase(DbContextOptions<DataBase> options) : DbContext(options)
     public DbSet<Flight> Flights { get; set; }
     public DbSet<Ticket> Tickets { get; set; }
 
-    
+    public DbSet<User> Users { get; set; }
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
 
+
+        modelBuilder.Entity<User>().HasIndex(u => u.Username).IsUnique();
+        modelBuilder.Entity<User>().HasIndex(u => u.Email).IsUnique();
+        modelBuilder.Entity<User>().Property(u => u.PasswordHash).IsRequired();
+        modelBuilder.Entity<User>().Property(u => u.Role).IsRequired();
+        FlightDb(modelBuilder);
+
+        TicketDb(modelBuilder);
+    }
+
+    private static void TicketDb(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Ticket>().Property(t => t.Price).IsRequired().HasColumnType("decimal(7,2)");
+        modelBuilder.Entity<Ticket>().Property(t => t.PassengerEmail).IsRequired();
+        modelBuilder.Entity<Ticket>().Property(t => t.Status).IsRequired();
+        modelBuilder.Entity<Ticket>().Property(t => t.PassengerName).IsRequired();
+        modelBuilder.Entity<Ticket>().Property(t => t.BookingDate).IsRequired();
+    }
+
+    private static void FlightDb(ModelBuilder modelBuilder)
+    {
         modelBuilder.Entity<Flight>()
             .HasMany(f => f.Tickets)
             .WithOne(t => t.Flight)
             .HasForeignKey(t => t.FlightId)
             .OnDelete(DeleteBehavior.Cascade);
 
-
-        modelBuilder.Entity<Flight>().Property(f => f.FlightNumber).IsUnicode().IsRequired();
+        modelBuilder.Entity<Flight>().HasIndex().IsUnique();
         modelBuilder.Entity<Flight>().Property(f => f.DepartureCity).IsRequired();
         modelBuilder.Entity<Flight>().Property(f => f.ArrivalCity).IsRequired();
         modelBuilder.Entity<Flight>().Property(f => f.DepartureTime).IsRequired();
         modelBuilder.Entity<Flight>().Property(f => f.ArrivalTime).IsRequired();
-
-        modelBuilder.Entity<Ticket>().Property(t => t.Price).IsRequired().HasColumnType("decimal(7,2)");
-        modelBuilder.Entity<Ticket>().Property(t => t.PassengerEmail).IsRequired();
-        modelBuilder.Entity<Ticket>().Property(t => t.Status).IsRequired();
-        modelBuilder.Entity<Ticket>().Property(t => t.PassengerName).IsRequired();
-        modelBuilder.Entity<Ticket>().Property(t => t.BookingDate).IsRequired();
     }
 }
--- FlightReservation/infra/models/User.cs
+++ FlightReservation/infra/models/User.cs
@@ -1,10 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FlightReservation.infra.models;
 
 public class User
 {
     public int Id { get; set; }
-    public string Username { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public string PasswordHash { get; set; } = string.Empty;
-    public Role Role { get; set; }
+    [MaxLength(20)] [MinLength(5)] public required string Username { get; set; }
+    [EmailAddress] public required string Email { get; set; }
+
+    [MaxLength(100, ErrorMessage = "password must be between 8 and 100 characters")]
+    [MinLength(8)]
+    public required string PasswordHash { get; set; }
+
+    public required Role Role { get; set; }
 }
